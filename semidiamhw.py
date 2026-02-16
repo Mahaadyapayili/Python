4da721c5a0a6e60db8251db4975aebff77b30f55
Mahaadyapayili <mahaadyapayili@gmail.com>
2026-02-16 08:06:42 +0530

    Add files via upload

---
+++ semidiamhw.py
@@ -0,0 +1,10 @@
+print("Half Diamond of Stars(*):")
+n = int(input("enter the number of rows:"))
+for i in range(n):
+    for j in range(i+1):
+        print("*",end="")
+    print()   
+for i in range(n):
+    for j in range(i-1):
+        print("*",end="")
+    print()   
