5b19e4e0fc5a1a011064be3fa726280da9cc0edf
Mahaadyapayili <mahaadyapayili@gmail.com>
2026-03-25 18:35:42 +0530

    Add files via upload

---
+++ Age_Calc.py
@@ -0,0 +1,9 @@
+i = int(input("Enter your Age : "))
+
+if (i==15) :
+    if (i > 20):
+        print("You cannot enrol in this class")
+    if (i < 10):
+        print ("You are too young for the class")
+    else:
+        print("We are happy to tell you that you can enroll !!")
