bf7e04396b595e68a1bd1069dae0a8fa014b8e71
Mahaadyapayili <mahaadyapayili@gmail.com>
2026-03-25 11:00:00 +0530

    Add files via upload

---
+++ Symmetric_Difference.py
@@ -0,0 +1,14 @@
+Set1={"Blue","Green"}
+Set2={"Blue","Yellow"}
+print(Set1)
+print(Set2)
+print("The Symmetric_difference of the first Two sets is :")
+Set3=Set1.symmetric_difference(Set2)
+print(Set3)
+seta={1,2,3,4,5}
+setb={1,5,6,7,8,9}
+print(seta)
+print(setb)
+print("The Symmetric_difference of the second Two sets is :")
+setx=seta.symmetric_difference(setb)
+print(setx)
